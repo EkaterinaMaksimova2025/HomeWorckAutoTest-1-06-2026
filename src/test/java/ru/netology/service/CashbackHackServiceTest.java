a7fc3110ee7a9d1984a1a6212d1054c287e2498e
Ekaterina <akai.gumba@gmail.com>
2026-06-25 00:01:02 +0300

    initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,85 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void acceptableAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+
+    }
+
+    /// Граничные значения
+    /// Не знаю, корректно ли в данном случае отрицательное значение. Поэтому -1 не пишу.
+
+    @Test
+    public void secondMinAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void thirdMinAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void fistMaxAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void secondMaxAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void thirdMaxAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+
+        int actual = service.remain(amount);
+        int expected = 999; ///т.к. пошло на вторую тысячу
+
+        assertEquals(expected, actual);
+
+    }
+}
